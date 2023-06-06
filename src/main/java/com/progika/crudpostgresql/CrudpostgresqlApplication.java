a43f659b1e302173bd76c631f1c0222543311529
Rod97139 <fayaflame@gmail.com>
2023-06-06 08:15:19 +0200

    init project

---
+++ src/main/java/com/progika/crudpostgresql/CrudpostgresqlApplication.java
@@ -0,0 +1,14 @@
+package com.progika.crudpostgresql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudpostgresqlApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World !");
+		SpringApplication.run(CrudpostgresqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/progika/crudpostgresql/CrudpostgresqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.progika.crudpostgresql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudpostgresqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
